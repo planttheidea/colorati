095d03b390b334b8610fe2bc69908dcea7d632fb
Tony Quetano <tony.quetano@planttheidea.com>
2025-11-15 23:17:42 +0100

    update to latest hash-it to make sure it works

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export const REPLACE_ME = {};
+import { hash } from 'hash-it';
+
+export function color<Value>(value: Value): string {
+  const hashed = hash(value).toString(16);
+
+  console.log(hashed);
+
+  return 'TO-DO';
+}
