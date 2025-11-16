3461cf7c13ed1adcd42d223fd2661b2e59a60368
Tony Quetano <tony.quetano@planttheidea.com>
2025-11-16 10:45:24 +0100

    use new rollup script to create configs

--- build/rollup/cjs.config.js
+++ build/rollup/cjs.config.js
@@ -1,3 +1,3 @@
-import { createRollupConfig } from '@planttheidea/build-tools';
+import { createRollupConfig } from '@planttheidea/build-tools/rollup';
 
 export default createRollupConfig({ outputFormat: 'cjs' });
